d47bc4c7d449348827724347ce7a55876b2e5861
Puru Vijay <awesomepuruvj@gmail.com>
2024-07-16 04:48:13 +0530

    Legacy dts for both

--- legacy-dts.ts
+++
@@ -1,8 +0,0 @@
-import { writeFile } from 'node:fs/promises';
-
-await writeFile(
-	'./dist/legacy/legacy.d.cts',
-	`declare const traverse: typeof import('..')['default'];
-export = traverse;
-`,
-);
