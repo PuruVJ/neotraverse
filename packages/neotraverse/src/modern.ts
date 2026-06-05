46eaa476ab41d0ae881e93f99928f6fdb87ce6f8
Puru Vijay <devpuruvj@gmail.com>
2026-06-05 18:43:42 +0530

    fix: add comment to clarify state update in walk_async function

--- packages/neotraverse/src/modern.ts
+++ packages/neotraverse/src/modern.ts
@@ -651,6 +651,7 @@ async function walk_async(
 		if (descend && ctx.circular === undefined) {
 			parents.push(ctx);
 
+			// bleh
 			if (node !== node0) update_state(ctx);
 
 			const keys = ctx.keys as PropertyKey[];
